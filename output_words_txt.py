7178a9ae13a1ca7da56cf0cea90341b0cf11c8b2
Tyan3001 <60082495+Tyan3001@users.noreply.github.com>
2020-05-19 18:39:51 -0400

    Add files via upload

---
+++ output_words_txt.py
@@ -0,0 +1,22 @@
+from Data_Handling import import_data
+
+
+Data = import_data()
+f1 = open('hate_words.txt', 'a')
+f2 = open('love_words.txt', 'a')
+
+for index, row in Data.iterrows():
+    if str(row['label']) == '1':
+        for i in row['Tweet_words']:
+            try:
+                f1.write(i + '\n')
+            except:
+                pass
+    else:
+        for i in row['Tweet_words']:
+            try:
+                f2.write(i + '\n')
+            except:
+                pass
+f1.close()
+f2.close()
